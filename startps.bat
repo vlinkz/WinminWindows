7325a57349e5171bf13132833d4a17b820578ebe
Victor Fuentes <hyruleterminatriforce@gmail.com>
2020-06-06 15:05:27 -0400

    Fix spaces
    Fixes issues described in https://github.com/vlinkz/winmin-tools/pull/1

--- startps.bat
+++ startps.bat
@@ -1,6 +1,9 @@
 @ECHO OFF
 ::start psexec64 -u vm -p vm -d -accepteula -i 1 cmd.exe /C start "" /MAX %1 %2
-schtasks /create /sc once /tn test /tr "cmd.exe /C start \"\" /MAX %1 %2" /ST 00:00
+if "%~2" == "" (
+    schtasks /create /sc once /tn test /tr "cmd.exe /C start \"\" /MAX \"%~1\"" /ST 00:00
+) else (
+    schtasks /create /sc once /tn test /tr "cmd.exe /C start \"\" /MAX \"%~1\" \"%~2\"" /ST 00:00
+)
 schtasks /run /i /TN test
 schtasks /delete /tn test /F
-
